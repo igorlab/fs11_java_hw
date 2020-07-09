5fbdeb9b6feddcc3d5fd73b8d9b88d75da1bb900
Igor Tkachenko <tigorlab@gmail.com>
2020-07-09 10:18:53 +0300

    1st HW refactoring

--- java-basic/HW_1/src/hw_1/NumbersHW.java
+++ java-basic/HW_1/src/hw_1/NumbersHW.java
@@ -11,17 +11,18 @@ public class NumbersHW {
     public static void main(String[] args) {
 
         int random = getRandomInt(0, 100);
-        System.out.printf("Your secret number: %s \n", random);
+        //System.out.printf("Your secret number: %s \n", random);
 
         System.out.print("Hi! Type your name: ");
         String userName = readString();
 
         System.out.printf("%s, Let the game begin! \n", userName);
 
-        int[] yourNumbers = new int[100];
+        int[] yourNumbers = new int[100]; // так как игрок не склеротик, а случайное число находится в диапазоне [0-100], то 100 элементов достаточно
         int i = 0;
 
-        while (i <= 100){
+        while (true){ // Сам процесс игры обрабатывается в бесконечном цикле
+            if (i > 99) break;
             int number = readIntNumbers();
             yourNumbers[i] = number;
             i++;
@@ -32,7 +33,6 @@ public class NumbersHW {
                     for (int n = 0; n < i - 1; n++) {
                         for (int j = 0; j < i - n - 1; j++) {
                             if (yourNumbers[j] < yourNumbers[j + 1]) {
-                                // меняем элементы местами
                                 temp = yourNumbers[j];
                                 yourNumbers[j] = yourNumbers[j + 1];
                                 yourNumbers[j + 1] = temp;
